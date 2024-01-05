b49dbca6168003d4ba99ee1b1960e264a35775cf
Abhishek Raut <102864290+abhishekraut1@users.noreply.github.com>
2024-01-06 02:07:15 +0530

    minor changes done.

--- frontend/src/components/UserAvatar/UserBadgeItem.js
+++ frontend/src/components/UserAvatar/UserBadgeItem.js
@@ -1,23 +1,27 @@
 import { CloseIcon } from '@chakra-ui/icons'
 import { Box } from '@chakra-ui/react'
 import React from 'react'
+import { ChatState } from '../../Context/ChatProvider'
 
-const UserBadgeItem = ({user,handleFunction}) => {
+const UserBadgeItem = ({ user, handleFunction }) => {
+  const { selectedChat } = ChatState()
   return (
     <Box
-    bg='purple'
-    px={2}
-    py={1}
-    m={1}
-    mb={2}
-    fontSize={12}
-    borderRadius='lg'
-    color='white'
-    cursor='pointer'
-    onClick={handleFunction}
+      style={selectedChat && (selectedChat.groupAdmin._id !== user._id) ?{
+        borderWidth: '2px', backgroundColor: 'white', color:'#0e61a1' , borderColor: '#0e61a1'
+      } : {backgroundColor: '#0e61a1'}}
+      px={2}
+      py={1}
+      m={1}
+      mb={2}
+      fontSize={14}
+      borderRadius='lg'
+      color='white'
+      cursor='pointer'
+      onClick={handleFunction}
 
     >
-      {user.name}
+      {user ? user.name : ""}
       <CloseIcon pl={1} />
     </Box>
   )
