c0e62f1f5c6f1708ef3670b3ea45e7d7bac05721
Abhishek Raut <102864290+abhishekraut1@users.noreply.github.com>
2024-01-09 16:07:34 +0530

    bas re ba.

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ const io = new Server(server,{
     pingTimeout: 60000,
     cors:{
         // origin: 'http://localhost:3000',
-        origin: "https://realtime-chatapp-3mrk.onrender.com",
+        origin: "https://chatapp-test-frontend.vercel.app",
         methods: ["GET", "POST"],
     }
 })
